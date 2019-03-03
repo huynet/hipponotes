8fbe07ee6ded303066a88bfb9f69e510fe6cfa86
huynet <cocwedc@gmail.com>
2019-03-02 22:58:36 -0600

    mao

--- components/AppStack/ScheduleScreen.js
+++ components/AppStack/ScheduleScreen.js
@@ -1,165 +1,60 @@
-// import React, { Component } from 'react';
-// import {
-//     SafeAreaView,
-//     Text,
-//     StyleSheet
-// } from 'react-native'
-// import { Agenda } from 'react-native-calendars'
+import React, { Component } from 'react';
+import {
+    Modal,
+    Text,
+    TouchableHighlight,
+    View,
+    Alert
+} from 'react-native';
 
-// import AgendaScreen from '../AgendaScreen'
+import CameraScreen from '../CameraScreen';
 
-// class ScheduleScreen extends Component {
-//     render() {
-//         return (
-//             <SafeAreaView style={styles.container}>
-//                 {/* <AgendaScreen /> */}
-//                 <Text style={styles.text}>
-//                     ScheduleScreen
-//                 </Text>
-//             </SafeAreaView>
-//         )
-//     }
-// }
-
-// export default ScheduleScreen;
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         justifyContent: 'center',
-//         alignItems: 'center'
-//     },
-//     text: {
-//         fontFamily: 'Avenir Next',
-//         fontSize: 20,
-//     }
-// })
-
-import React from "react";
-import { 
-    Text, 
-    View, 
-    TouchableOpacity,
-    Image
-} from "react-native";
-import { 
-    Camera, 
-    Permissions 
-} from "expo";
-
-export default class CameraExample extends React.Component {
+class ScheduleScreen extends Component {
     state = {
-        hasCameraPermission: null,
-        type: Camera.Constants.Type.back
+        modalVisible: false,
     };
 
-    async componentDidMount() {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA);
-        this.setState({ hasCameraPermission: status === "granted" });
+    setModalVisible(visible) {
+        this.setState({ modalVisible: visible });
     }
-
-    snap = async () => {
-        console.log("TEST")
-        if (this.camera) {
-          let photo = await this.camera.takePictureAsync();
-          console.log(photo)
-        } else (
-            console.log("not a cam")
-        )
-      };
 
     render() {
-        const { hasCameraPermission } = this.state;
-        if (hasCameraPermission === null) {
-            return <View />;
-        } else if (hasCameraPermission === false) {
-            return <Text>No access to camera</Text>;
-        } else {
-            return (
-                <View style={{ flex: 1 }}>
-                    <Camera 
-                        style={{ flex: 1 }} 
-                        type={this.state.type}
-                        ref={ref => { this.camera = ref }}
-                    >
-                        <View
-                            style={{
-                                flex: 1,
-                                backgroundColor: "transparent",
-                                flexDirection: "row"
-                            }}
-                        >
-                            <TouchableOpacity
-                                style={{
-                                    flex: 0.1,
-                                    alignSelf: "flex-end",
-                                    alignItems: "center"
-                                }}
+        return (
+            <View style={{ marginTop: 22 }}>
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                    }}>
+                    <View style={{ marginTop: 22 }}>
+                        <View>
+                            <Text>Hello World!</Text>
+
+                            <TouchableHighlight
                                 onPress={() => {
-                                    this.setState({
-                                        type:
-                                            this.state.type === Camera.Constants.Type.back
-                                                ? Camera.Constants.Type.front
-                                                : Camera.Constants.Type.back
-                                    });
+                                    this.setModalVisible(!this.state.modalVisible);
                                 }}
                             >
-                                <Text
-                                    style={{ fontSize: 18, marginBottom: 10, color: "white" }}
-                                >
-                                    {" "}
-                                    Flip{" "}
-                                </Text>
-                            </TouchableOpacity>
-
-                            {/* Snap */}
-                            <TouchableOpacity
-                                style={{
-                                    flex: 1,
-                                    alignSelf: "flex-end",
-                                    alignItems: "center",
-                                    justifyContent: 'center',
-                                }}
-                                onPress={() => {
-                                    this.snap();
-                                }}
-                            >
-                                <Image
-                                    style={{
-                                       height: 60,
-                                       width: 60 
-                                    }}
-                                    source={{uri: 'http://icons.iconarchive.com/icons/martz90/circle/256/video-camera-icon.png'}}
-                                />
-                            </TouchableOpacity>
-
-                            <TouchableOpacity
-                                style={{
-                                    flex: 0.1,
-                                    alignSelf: "flex-end",
-                                    alignItems: "center"
-                                }}
-                                onPress={() => {
-                                    this.setState({
-                                        type:
-                                            this.state.type === Camera.Constants.Type.back
-                                                ? Camera.Constants.Type.front
-                                                : Camera.Constants.Type.back
-                                    });
-                                }}
-                            >
-                                <Text
-                                    style={{ fontSize: 18, marginBottom: 10, color: "white" }}
-                                >
-                                    {" "}
-                                    Flip{" "}
-                                </Text>
-                            </TouchableOpacity>
-
+                                
+                                <Text>Hide Modal</Text>
+                            </TouchableHighlight>
                         </View>
-                    </Camera>
-                </View>
-            );
-        }
+                    </View>
+                </Modal>
+
+                <TouchableHighlight
+                    style={{ marginTop: 200 }}
+                    onPress={() => {
+                        this.setModalVisible(true);
+                    }}>
+                    {/* <CameraScreen /> */}
+                    <Text>Show Modal</Text>
+                </TouchableHighlight>
+            </View>
+        );
     }
 }
+
+export default ScheduleScreen;
