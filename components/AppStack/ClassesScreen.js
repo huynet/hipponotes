2dc5c647490108cd6280ccc1fc07146defe72bb4
huynet <cocwedc@gmail.com>
2019-03-04 11:04:39 -0600

    styling

--- components/AppStack/ClassesScreen.js
+++ components/AppStack/ClassesScreen.js
@@ -1,116 +1,119 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView } from 'react-native';
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    ScrollView,
+    SafeAreaView,
+} from 'react-native'
 import ListCard from '../ListCard'
 import SquareCard from '../SquareCard'
 
 class ClassesScreen extends Component {
+    static navigationOptions = {
+        header: null,
+    }
 
-   static navigationOptions = {
-      header: null
-   }
+    render() {
+        return (
+            <View style={styles.container}>
+                <ScrollView
+                    style={styles.container}
+                    showsVerticalScrollIndicator={false}
+                >
+                    <View style={styles.titleBar}>
+                        <Image
+                            style={styles.avatar}
+                            source={require('../../assets/huy.jpg')}
+                        />
+                        <Text style={styles.title}>Welcome back,</Text>
+                        <Text style={styles.name}>Huy Pham</Text>
+                    </View>
 
-   render() {
-      return (
-         <View style={styles.container}>
-            <ScrollView
-               style={styles.container}
-               showsVerticalScrollIndicator={false}
-            >
-               <View style={styles.titleBar}>
-                  <Image style={styles.avatar} source={require('../../assets/huy.jpg')} />
-                  <Text style={styles.title}>Welcome back,</Text>
-                  <Text style={styles.name}>Huy Pham</Text>
-               </View>
+                    <Text style={styles.subTitle}>Your classes</Text>
 
-               <Text style={styles.subTitle}>
-                  Your classes
-               </Text>
-
-               <SafeAreaView
-                  style={styles.classes}
-               >
-                  <ListCard
-                     title="CS2336"
-                     color="#E8384F"
-                     desc="Introduction to Computer Science 2"
-                  />
-                  <ListCard
-                     title="CS2305"
-                     color="#FD612C"
-                     desc="Discrete Mathematics"
-                  />
-                  <ListCard
-                     title="MATH2418"
-                     color="#A4CF30"
-                     desc="Linear Algebra"
-                  />
-                  <ListCard
-                     title="MATH2419"
-                     color="#62D26F"
-                     desc="Calculus 2"
-                  />
-                  <ListCard
-                     title="PHIL1301"
-                     color="#4186E0"
-                     desc="Intro to Philosophy"
-                  />
-               </SafeAreaView>
-            </ScrollView>
-         </View>
-      );
-   }
+                    <SafeAreaView style={styles.classes}>
+                        <ListCard
+                            title="CS2336"
+                            color="#E8384F"
+                            desc="Introduction to Computer Science 2"
+                        />
+                        <ListCard
+                            title="CS2305"
+                            color="#FD612C"
+                            desc="Discrete Mathematics"
+                        />
+                        <ListCard
+                            title="MATH2418"
+                            color="#A4CF30"
+                            desc="Linear Algebra"
+                        />
+                        <ListCard
+                            title="MATH2419"
+                            color="#62D26F"
+                            desc="Calculus 2"
+                        />
+                        <ListCard
+                            title="PHIL1301"
+                            color="#4186E0"
+                            desc="Intro to Philosophy"
+                        />
+                    </SafeAreaView>
+                </ScrollView>
+            </View>
+        )
+    }
 }
 
-export default ClassesScreen;
+export default ClassesScreen
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: 'rgb(255, 255, 255)'
-   },
-   cover: {
-
-   },
-   avatar: {
-      width: 44,
-      height: 44,
-      backgroundColor: 'black',
-      borderRadius: 22,
-      marginLeft: 30,
-      position: 'absolute',
-      top: 0,
-      left: 0
-   },
-   titleBar: {
-      width: '100%',
-      marginTop: 50,
-      paddingLeft: 90,
-   },
-   title: {
-      fontSize: 16,
-      fontFamily: 'Avenir Next',
-      color: '#b8bece',
-      fontWeight: '500',
-   },
-   subTitle: {
-      fontSize: 15,
-      fontFamily: 'Avenir Next',
-      color: '#b8bece',
-      fontWeight: '600',
-      marginLeft: 30,
-      marginTop: 25,
-      textTransform: 'uppercase'
-   },
-   name: {
-      fontSize: 20,
-      fontFamily: 'Avenir Next',
-      color: '#3c4560',
-      fontWeight: '700',
-      marginTop: -3
-   },
-   classes: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-evenly',
-   }
-});
+    container: {
+        flex: 1,
+        backgroundColor: 'rgb(255, 255, 255)',
+    },
+    cover: {},
+    avatar: {
+        width: 44,
+        height: 44,
+        backgroundColor: 'black',
+        borderRadius: 22,
+        marginLeft: 30,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+    },
+    titleBar: {
+        width: '100%',
+        marginTop: 50,
+        paddingLeft: 90,
+    },
+    title: {
+        fontSize: 16,
+        fontFamily: 'Avenir Next',
+        color: '#b8bece',
+        fontWeight: '500',
+    },
+    subTitle: {
+        fontSize: 15,
+        fontFamily: 'Avenir Next',
+        color: '#b8bece',
+        fontWeight: '600',
+        marginLeft: 30,
+        marginTop: 25,
+        textTransform: 'uppercase',
+    },
+    name: {
+        fontSize: 20,
+        fontFamily: 'Avenir Next',
+        color: '#3c4560',
+        fontWeight: '700',
+        marginTop: -3,
+    },
+    classes: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+    },
+})
--- components/ListCard.js
+++ components/ListCard.js
@@ -1,71 +1,71 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    TouchableOpacity,
+} from 'react-native'
 
-import { withNavigation } from 'react-navigation';
+import { withNavigation } from 'react-navigation'
 
-const _margin = 30;
-const _borderRadius = 8;
-const _width = Dimensions.get("window").width - _margin * 2
+const _margin = 30
+const _borderRadius = 8
+const _width = Dimensions.get('window').width - _margin * 2
 const _height = 70
 
 class ListCard extends Component {
-   render() {
-      const { title, color, desc } = this.props
-      return (
-         <TouchableOpacity
-            style={[styles.container, { backgroundColor: color }]}
-            onPress={() => {
-               this.props.navigation.navigate('Detail', {
-                  title,
-                  color,
-                  desc
-               })
-            }}
-         >
-            <View style={styles.cover}>
-               <Text style={styles.title}>
-                  {title}
-               </Text>
-               <Text style={styles.desc}>
-                  {desc}
-               </Text>
-            </View>
-         </TouchableOpacity>
-      )
-   }
+    render() {
+        const { title, color, desc } = this.props
+        return (
+            <TouchableOpacity
+                style={[styles.container, { backgroundColor: color }]}
+                onPress={() => {
+                    this.props.navigation.navigate('Detail', {
+                        title,
+                        color,
+                        desc,
+                    })
+                }}
+            >
+                <View style={styles.cover}>
+                    <Text style={styles.title}>{title}</Text>
+                    <Text style={styles.desc}>{desc}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
 }
 
-export default withNavigation(ListCard);
+export default withNavigation(ListCard)
 
 const styles = StyleSheet.create({
-   container: {
-      width: _width,
-      height: _height,
-      borderRadius: _borderRadius,
-      marginTop: 15,
+    container: {
+        width: _width,
+        height: _height,
+        borderRadius: _borderRadius,
+        marginTop: 15,
 
-      shadowOpacity: 0.125,
-      shadowRadius: 5,
-      shadowColor: 'black',
-      shadowOffset: { height: 2, width: 2 },
-   },
-   cover: {
-
-   },
-   title: {
-      fontSize: 20,
-      fontFamily: 'Avenir Next',
-      color: 'white',
-      fontWeight: '600',
-      margin: 10,
-      marginBottom: 0
-   },
-   desc: {
-      fontSize: 14,
-      fontFamily: 'Avenir Next',
-      color: 'white',
-      marginLeft: 10,
-      marginRight: 10,
-      fontWeight: '600'
-   }
-});
+        shadowOpacity: 0.125,
+        shadowRadius: 5,
+        shadowColor: 'black',
+        shadowOffset: { height: 2, width: 2 },
+    },
+    cover: {},
+    title: {
+        fontSize: 20,
+        fontFamily: 'Avenir Next',
+        color: 'white',
+        fontWeight: '600',
+        margin: 10,
+        marginBottom: 0,
+    },
+    desc: {
+        fontSize: 14,
+        fontFamily: 'Avenir Next',
+        color: 'white',
+        marginLeft: 10,
+        marginRight: 10,
+        fontWeight: '600',
+    },
+})
