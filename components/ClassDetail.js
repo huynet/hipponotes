e4bd3f9504b1a740e2d7f3416f6726fcd086750d
huynet <cocwedc@gmail.com>
2019-04-03 21:36:25 -0500

    data manipulation

--- components/ClassDetail.js
+++ components/ClassDetail.js
@@ -1,89 +1,128 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-   SectionList,
-   StyleSheet,
-   Text,
-   TextInput,
-   View,
-   TouchableOpacity
-} from 'react-native';
+    SectionList,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    TouchableOpacity,
+} from 'react-native'
 
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons'
 
 class ClassDetail extends Component {
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: navigation.getParam('title', ''),
+            headerRight: (
+                <TouchableOpacity style={{ paddingRight: 10 }}>
+                    <Ionicons name="ios-add" color="#007AFF" size={36} />
+                </TouchableOpacity>
+            ),
+        }
+    }
 
-   static navigationOptions = ({ navigation }) => {
-      return {
-         title: navigation.getParam('title', ''),
-         headerRight: (
-            <TouchableOpacity
-               style={{ paddingRight: 10 }}
-            >
-               <Ionicons name="ios-add" color='#007AFF' size={36} />
-            </TouchableOpacity>
-         )
-      }
-   }
+    state = {
+        course: [
+            {
+                title: 'Week 1',
+                data: [
+                    '1.1 Calculus Review',
+                    '1.2 Matrix Operations',
+                    '2.1 Dot Product',
+                    '2.2 Plane Systems',
+                ],
+            },
+            {
+                title: 'Week 2',
+                data: [
+                    '2.3 Partial Derivatives',
+                    '2.4 Limits',
+                    '2.5 Determinants',
+                ],
+            },
+            { 
+				title: 'Family', 
+				data: [
+					'Emma', 
+					'Emi', 
+					'Emiliano', 
+					'Ernesto'
+				] 
+			},
+            {
+                title: 'Friends',
+                data: [
+                    'Jackson',
+                    'James',
+                    'Jillian',
+                    'Jimmy',
+                    'Joel',
+                    'John',
+                    'Julie',
+                ],
+            },
+        ],
+    }
 
-   render() {
-      return (
-         <View style={styles.container}>
-            <TextInput
-               style={styles.searchBar}
-               // value={this.state.searchText}
-               // onChange={this.setSearchText.bind(this)}
-               placeholder='Search State'
-            />
-            <SectionList
-               sections={[
-                  { title: 'Week 1', data: ['1.1 Calculus Review', '1.2 Matrix Operations', '2.1 Dot Product', '2.2 Plane Systems'] },
-                  { title: 'Week 2', data: ['2.3 Partial Derivatives', '2.4 Limits', '2.5 Determinants'] },
-                  { title: 'Family', data: ['Emma', 'Emi', 'Emiliano', 'Ernesto'] },
-                  { title: 'Friends', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie'] },
-               ]}
-               renderItem={({ item }) => (
-                  <TouchableOpacity>
-                     <Text style={styles.item}>{item}</Text>
-                  </TouchableOpacity>
-               )}
-               renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-               keyExtractor={(item, index) => index}
-            />
-         </View>
-      );
-   }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.searchBar}
+                    // value={this.state.searchText}
+                    // onChange={this.setSearchText.bind(this)}
+                    placeholder="Search State"
+                />
+                <SectionList
+                    sections={this.state.course}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity>
+                            <Text style={styles.item}>{item}</Text>
+                        </TouchableOpacity>
+                    )}
+                    renderSectionHeader={({ section }) => (
+                        <Text style={styles.sectionHeader}>
+                            {section.title}
+                        </Text>
+                    )}
+                    keyExtractor={(item, index) => index}
+                />
+            </View>
+        )
+    }
 }
 
-export default ClassDetail;
+export default ClassDetail
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1
-   },
-   sectionHeader: {
-      paddingTop: 5,
-      paddingLeft: 15,
-      paddingRight: 10,
-      paddingBottom: 5,
-      fontSize: 18,
-      fontFamily: 'Avenir Next',
-      fontWeight: 'bold',
-      backgroundColor: 'rgba(247,247,247,1.0)',
-   },
-   item: {
-      padding: 10,
-      paddingLeft: 15,
-      fontFamily: 'Avenir Next',
-      fontWeight: '500',
-      fontSize: 18,
-      height: 44,
-   },
-   searchBar: {
-      //borderWidth: 1,
-      height: 40,
-      fontFamily: 'Avenir Next',
-      fontSize: 16,
-      fontWeight: '500',
-      paddingLeft: 12
-   }
+    container: {
+        flex: 1,
+    },
+    sectionHeader: {
+        paddingTop: 5,
+        paddingLeft: 15,
+        paddingRight: 10,
+        paddingBottom: 5,
+        fontSize: 18,
+        fontFamily: 'Avenir Next',
+        fontWeight: 'bold',
+        backgroundColor: 'rgba(247,247,247,1.0)',
+    },
+    item: {
+        padding: 10,
+        paddingLeft: 15,
+        fontFamily: 'Avenir Next',
+        fontWeight: '500',
+        fontSize: 18,
+        height: 44,
+    },
+    searchBar: {
+        //borderWidth: 1,
+        height: 40,
+        fontFamily: 'Avenir Next',
+        fontSize: 16,
+        fontWeight: '500',
+        paddingLeft: 12,
+    },
 })
